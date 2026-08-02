5832d232dae505905cba60c8f0e0ac44075910d6
ARJUN GHUNGRUD <ghungrudarjun@gmail.com>
2026-08-02 14:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (25.89%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int helper(int idx,int b,int cap,vector<int>& nums,vector<vector<vector<int>>>& dp){
-        if(idx>=nums.size() || cap==0)return 0;
-        if(dp[idx][b][cap]!=-1)return dp[idx][b][cap];
-        if(b==0){
-            return dp[idx][b][cap]=max(-nums[idx]+helper(idx+1,1-b,cap,nums,dp),helper(idx+1,b,cap,nums,dp));
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int buyP=INT_MAX;
+        int maxPro=0;
+        for(int i=1;i<n;i++){
+            buyP=min(buyP,prices[i-1]);
+            maxPro=max(maxPro,prices[i]-buyP);
         }
-        return dp[idx][b][cap]=max(nums[idx]+helper(idx+1,1-b,cap-1,nums,dp),helper(idx+1,b,cap,nums,dp));
-    }
-    int maxProfit(vector<int>& pri) {
-        int n=pri.size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(2, -1))
-        );
-        return helper(0,0,1,pri,dp);
+        return maxPro;
     }
 };
