55a780f71826eb5eeb8245c2f8c676f692bd22d3
ARJUN GHUNGRUD <ghungrudarjun@gmail.com>
2026-08-03 11:21:00 +0530

    Time: 8 ms (59.23%) | Memory: 45 MB (12.50%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -4,14 +4,16 @@ public:
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int child=0;
+        int count=0;
         int cookie=0;
         while(child<g.size() && cookie<s.size()){
             if(g[child]<=s[cookie]){
+                count++;
                 child++;
                 cookie++;
             }
             else cookie++;
         }
-        return child;
+        return count;
     }
 };
