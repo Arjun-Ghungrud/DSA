af136f522831e788e9c818f83149357d5b9cf7ef
ARJUN GHUNGRUD <ghungrudarjun@gmail.com>
2026-08-15 11:59:31 +0530

    Time: 3 ms (40.14%) | Memory: 8.3 MB (47.65%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool issafe(vector<string>&board,int n,int row,int col){
+        //horizantal
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q')return false;
+        }
+        //vertical
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q')return false;
+        }
+        //digonal left
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q')return false;
+        }
+        //digonal right
+        for(int i=row,j=col;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q')return false;
+        }
+        return true;
+    }
+    void Nqueenconfi(vector<string>&board,int &ans,int n,int row){
+        if(row==n){
+            ans++;
+            return ;
+        }
+        for(int j=0;j<n;j++){
+            if(issafe(board,n,row,j)){
+                board[row][j]='Q';
+                Nqueenconfi(board,ans,n,row+1);
+                board[row][j]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        int ans=0;
+        Nqueenconfi(board,ans,n,0);
+        return ans;
+    }
+};
