a12d9e8c1038e1f43479df0efca837fa2acc112b
ARJUN GHUNGRUD <ghungrudarjun@gmail.com>
2026-08-15 11:37:25 +0530

    Time: 6 ms (12.76%) | Memory: 16.4 MB (15.37%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<string>res;
-    void helper(string curr,int open,int close,int n){
+    
+    void helper(string curr,vector<string>&res,int open,int close,int n){
         if(curr.size()==2*n){
             res.push_back(curr);
             return;
         }
         if(open<n){
             curr.push_back('(');
-            helper(curr,open+1,close,n);
+            helper(curr,res,open+1,close,n);
             curr.pop_back();
         }
         if(close<open){
             curr.push_back(')');
-            helper(curr,open,close+1,n);
+            helper(curr,res,open,close+1,n);
             curr.pop_back();
         }
         return;
@@ -22,7 +22,8 @@ public:
         string curr="";
         int open=0;
         int close=0;
-        helper(curr,open,close,n);
+        vector<string>res;
+        helper(curr,res,open,close,n);
         return res;
     }
 };
