b1320b4879dfd2b9e4a0e66f1a872097a1f8a71f
ARJUN GHUNGRUD <ghungrudarjun@gmail.com>
2026-08-20 10:49:37 +0530

    Time: 1 ms (32.96%) | Memory: 23.9 MB (45.52%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -9,13 +9,7 @@ public:
             if(arr1[arr1.size()-1]>arr2[arr2.size()-1])arr1.push_back(nums[i]);
             else arr2.push_back(nums[i]);
         }
-        vector<int>ans;
-        for(int i=0;i<arr1.size();i++){
-            ans.push_back(arr1[i]);
-        }
-        for(int i=0;i<arr2.size();i++){
-            ans.push_back(arr2[i]);
-        }
-        return ans;
+        arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+        return arr1;
     }
 };
